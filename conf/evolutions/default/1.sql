1cece2ac0890cdb096c25fc74ca9f03cc904dad6
RemyG <remy.gardette@gmail.com>
2013-12-10 16:36:46 +0000

    Added sql evolution

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,14 +3,26 @@
 
 # --- !Ups
 
+create table section (
+  id                        bigint not null,
+  label                     varchar(255),
+  constraint pk_section primary key (id))
+;
+
 create table task (
   id                        bigint not null,
   label                     varchar(255),
+  description               varchar(255),
+  section_id                bigint,
   constraint pk_task primary key (id))
 ;
 
+create sequence section_seq;
+
 create sequence task_seq;
 
+alter table task add constraint fk_task_section_1 foreign key (section_id) references section (id) on delete restrict on update restrict;
+create index ix_task_section_1 on task (section_id);
 
 
 
@@ -18,9 +30,13 @@ create sequence task_seq;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
+drop table if exists section;
+
 drop table if exists task;
 
 SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists section_seq;
 
 drop sequence if exists task_seq;
 
